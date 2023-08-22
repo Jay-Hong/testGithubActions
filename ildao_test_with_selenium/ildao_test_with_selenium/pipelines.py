32a0178a3c5ede45bbdd6ea7345d29edf5828e91
Jay Hong <hjpyooo@gmail.com>
2023-08-22 23:26:54 +0900

    site 가 null 일경우 Drop

--- ildao_test_with_selenium/ildao_test_with_selenium/pipelines.py
+++ ildao_test_with_selenium/ildao_test_with_selenium/pipelines.py
@@ -9,32 +9,35 @@ from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 import re
 
-pattern0 = re.compile('일급 1[0-4]')
-pattern1 = re.compile('월급 1[0-4]')
-pattern2 = re.compile('일다오')
-pattern3 = re.compile('일급 1[0-6]')
-pattern4 = re.compile('월급 1[0-6]')
-pattern5 = re.compile('비계/동바리')
-pattern6 = re.compile('협의')
+pattern_day10_14 = re.compile('일급 1[0-4]')
+pattern_month10_14 = re.compile('월급 1[0-4]')
+pattern_ildao = re.compile('일다오')
+pattern_day10_16 = re.compile('일급 1[0-6]')
+pattern_month10_16 = re.compile('월급 1[0-6]')
+pattern_system = re.compile('비계/동바리')
+pattern_nego = re.compile('협의')
+pattern_null = re.compile('null')
 
 class IldaoTestWithSeleniumPipeline:
     def process_item(self, item, spider):
-        if len(pattern0.findall(item['pay'])) > 0:  # '일급 10 ~ 14' 들어가면 다 뺌
+        if len(pattern_day10_14.findall(item['pay'])) > 0:  # '일급 10 ~ 14' 들어가면 다 뺌
             raise DropItem('Drop 일급 10 ~ 14 !')
-        elif len(pattern1.findall(item['pay'])) > 0:  # '월급 10 ~ 14' 들어가면 다 뺌
+        elif len(pattern_month10_14.findall(item['pay'])) > 0:  # '월급 10 ~ 14' 들어가면 다 뺌
             raise DropItem('Drop 월급 10 ~ 14 !')
         elif item['title'].find('일다오') != -1:    # str.find('문자열') 찾았으면 0 ~ 찾은 첫번째 인댁스 / 못찾았으면 -1 반환
             raise DropItem('Drop title : 일다오')
-        elif len(pattern2.findall(item['detail'])) > 0: # 일부러 str.find와 다르게 해봄
+        elif len(pattern_ildao.findall(item['detail'])) > 0: # 일부러 str.find와 다르게 해봄
             raise DropItem('Drop detail : 일다오')
         elif len(item['detail']) < 35: # detail 35자 미만은 빼
             raise DropItem('Drop detail : detail 35자 미만')
         elif len(item['title']) < 6: # title 6자 미만은 빼
             raise DropItem('Drop detail : title 6자 미만')
-        elif (len(pattern3.findall(item['pay'])) > 0 or len(pattern4.findall(item['pay'])) > 0) and len(pattern5.findall(item['type'])) > 0:
+        elif (len(pattern_day10_16.findall(item['pay'])) > 0 or len(pattern_month10_16.findall(item['pay'])) > 0) and len(pattern_system.findall(item['type'])) > 0:
             raise DropItem('Drop detail : 비계/동바리 이면서 단가 16이하')
-        elif len(pattern6.findall(item['pay'])) > 0 and len(pattern5.findall(item['type'])) > 0:
+        elif len(pattern_nego.findall(item['pay'])) > 0 and len(pattern_system.findall(item['type'])) > 0:
             raise DropItem('Drop detail : 비계/동바리 이면서 협의')
+        elif len(pattern_null.findall(item['site'])) > 0:   # site 에 null 이 들어가면 빼
+            raise DropItem('Drop detail : site 가 null')
         else:
             return item
 
